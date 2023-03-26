0bdb840887409981acd92faa732cbb49c0d96560
Rigesand <83226147+Rigesand@users.noreply.github.com>
2023-03-26 21:51:06 +0300

    feat: Добавлен GroupController

---
+++ Backend/StudentAttendance.Api/Controllers/Groups/GroupController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAttendance.Core.Domains.Groups.Services;
+
+namespace StudentAttendance.Api.Controllers.Groups;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class GroupController : ControllerBase
+{
+    private readonly IGroupService _groupService;
+
+    public GroupController(IGroupService groupService)
+    {
+        _groupService = groupService;
+    }
+
+    [HttpPost]
+    public async Task CreateGroup(int groupNumber)
+    {
+        await _groupService.CreateGroup(groupNumber);
+    }
+}
